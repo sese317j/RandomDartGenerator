73756cb37cddfa557d0be1694463ca6b4f50592e
sese317j <sese.menne@web.de>
2022-03-18 15:05:34 +0100

    Wurf: toString gibt jetzt double oder tripple und Wurf aus und nicht die Punktzahl. 0 kann nicht double sein. Kommentare verbessert

--- src/com/jenns/Wurf.java
+++ src/com/jenns/Wurf.java
@@ -12,12 +12,15 @@ public class Wurf {
 
         int rand_wurf = rand.nextInt(22); // Zahlen von 0 bis 21
 
-        if(rand_wurf == 21){ // Dart 0 - 20 + 25
+        if(rand_wurf == 21){ // Dart 0 bis 20 + 25
             rand_wurf = 25;
-            multiplikator = rand2.nextInt(2) + 1; // Zahl von 1-2
+            multiplikator = rand2.nextInt(2) + 1; // Zahl von 1 bis 2
+        }
+        else if(rand_wurf == 0){
+            multiplikator = 1;
         }
         else {
-            multiplikator = rand2.nextInt(3) + 1; // Zahl von 1-3
+            multiplikator = rand2.nextInt(3) + 1; // Zahl von 1 bis 3
         }
         this.wurf = rand_wurf;
     }
@@ -49,6 +52,19 @@ public class Wurf {
 
     @Override
     public String toString(){
-        return getPunkte() + "";
+        String mult;
+        switch (multiplikator){
+            case 2:
+                mult= "d_";
+                break;
+            case 3:
+                mult="t_";
+                break;
+            default:
+                mult="";
+                break;
+        }
+        return mult + wurf;
     }
 }
+

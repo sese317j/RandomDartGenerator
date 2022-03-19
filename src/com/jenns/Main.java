0e9cac812c998d441ae2738dfee210bb33942052
sese317j <sese.menne@web.de>
2022-03-19 02:49:18 +0100

    Main: Support für mehrere Legs und Sets hinzugefügt.

--- src/com/jenns/Main.java
+++ src/com/jenns/Main.java
@@ -8,11 +8,21 @@ public class Main {
         final int punkte_initial = 501;
         final int multiplikator_out = 2;
         final boolean debug = true;
-        final int anzahl_sets = 1;
-        final int anzahl_legs = 1;
+        final int anzahl_sets = 2;
+        final int anzahl_legs = 3;
+
+        List<List<Leg>> all_sets = new ArrayList<>();
+
+        for(int i = 0; i < anzahl_sets; i++){
+            List<Leg> legList = new ArrayList<>();
+            for(int j = 0; j < anzahl_legs; j++){
+                Leg einLeg = new Leg( debug, multiplikator_out, punkte_initial);
+                legList.add(einLeg);
+            }
+            all_sets.add(legList);
+        }
 
 
-        Leg einLeg = new Leg( debug, multiplikator_out, punkte_initial);
 
     }
 
